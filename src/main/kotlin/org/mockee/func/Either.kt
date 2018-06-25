f59d4568c9e608117eb3b818706668b3528f661e
ramanallamilli <rama.nallamilli@gmail.com>
2018-06-25 20:52:00 +0100

    Optimised Either map/flatMap, cast to target type instead of creating a new instance.

--- src/main/kotlin/org/mockee/func/Either.kt
+++ src/main/kotlin/org/mockee/func/Either.kt
@@ -1,25 +1,26 @@
 package org.mockee.func
 
+@Suppress("UNCHECKED_CAST")
 sealed class Either<T, out R> {
 
     fun <N> leftMap(fn: (T) -> N): Either<N, R> {
         return when (this) {
             is Left -> Left(fn(leftValue))
-            is Right -> Right(rightValue)
+            is Right -> this as Either<N, R>
         }
     }
 
     fun <N> map(fn: (R) -> N): Either<T, N> {
         return when (this) {
             is Right -> Right(fn(rightValue))
-            is Left -> Left(leftValue)
+            is Left -> this as Either<T, N>
         }
     }
 
     fun <N> flatMap(fn: (R) -> Either<T, N>): Either<T, N> {
         return when (this) {
             is Right -> fn(rightValue)
-            is Left -> Left(leftValue)
+            is Left -> this as Either<T, N>
         }
     }
 
